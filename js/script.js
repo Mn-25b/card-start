6b9729339dbf30a0cb351267f9e0af73cdc16d11
Mn-25b <76849040+Mn-25b@users.noreply.github.com>
2021-02-20 18:49:39 +0300

    start-card v0.1
    пофикшен баг с закрытием карточки когда она открыта и на ее кликают, добавлены некоторые стили

--- js/script.js
+++ js/script.js
@@ -1,48 +1,32 @@
-let wrapper = document.querySelector('.wrapper')
+const wrapper = document.querySelector('.wrapper')
 wrapper.onclick = function (event) {
 
     let card = event.target.closest('.card');
-    //проверяет произош ли клик вне card
-    if (!card) {
-        closeAll()
-        return;
-    }
-    if (!wrapper.contains(card)) {
-        closeAll()
-        return;
-    }
 
+    //проверка на клик если !card то закрыть все
+    if (!card) closeAllCard();
+    if (!wrapper.contains(card)) closeAllCard();
 
     let front = card.children[0];
     let back = card.children[1];
 
-
-    if (card.classList.contains('pol')) {
-        closeMenu(front, back)
-        card.classList.toggle('pol')
-        return
-    }
-    if (!card.classList.contains('pol')) {
-        closeAll()
-        openMenu(card, front, back)
-        card.classList.toggle('pol')
+    if (card.classList.contains('card-active')) closeAllCard();
+    
+    if (!card.classList.contains('card-active')) {
+        closeAllCard()
+        openMenu(front, back)
+        card.classList.add('card-active')
     }
 };
 
-function openMenu(card, front, back) {
-
+function openMenu(front, back) {
     front.style.transform = 'rotateY(180deg)'
     back.style.transform = 'rotateY(360deg)'
 }
-function closeMenu(front, back) {
-    front.style.transform = 'rotateY(0deg)'
-    back.style.transform = 'rotateY(180deg)'
 
-}
-
-function closeAll(){
+function closeAllCard() {
     [].forEach.call(document.querySelectorAll('.card'), function (e) {
-        e.classList.remove('pol')
+        e.classList.remove('card-active')
         e.children[0].style.transform = 'rotateY(0deg)'
         e.children[1].style.transform = 'rotateY(180deg)'
     });
